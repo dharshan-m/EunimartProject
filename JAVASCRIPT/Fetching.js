c1a49c5c080962df8cf749069a4eda047d4e5de8
dharshan-m <dharshanmohan2704@gmail.com.com>
2023-02-24 11:58:17 +0530

    created the 2 page and styled the css

--- JAVASCRIPT/Fetching.js
+++ JAVASCRIPT/Fetching.js
@@ -9,16 +9,21 @@ printDetails = (display) => {
     const getinfo =  display.map((result) => {
         console.log(result);
         return `
-            <div class="maindiv">
+            <div class="userinfo">
+                <div class="maindiv">
                 <img src=${result.avatar} class="image">
-                <div class="seconddiv">
-                    <h2 class="sno">Id : ${result.id}</h2>
-                    <h2 class="fname">LastNmae : ${result.first_name}</h2>
-                    <h2 class="lname">FirstName : ${result.last_name}</h2>
-                    <h2 class="email">email : ${result.email}</h2>
-                </div> 
-                
+                    <div class="seconddiv">
+                        <h2 class="sno">Id : ${result.id}</h2><br>
+                        <h2 class="lname">FirstName : ${result.last_name}</h2><br>
+                        <h2 class="fname">LastNmae : ${result.first_name}</h2><br>
+                        <h2 class="email">email : ${result.email}</h2>
+                    </div> 
+                </div>
             </div>`
         });
     document.querySelector('.sub-container').innerHTML = getinfo;
 };
+
+function getBack(){
+    window.location.replace("http://127.0.0.1:5500/HTML/index.html");
+}
--- JAVASCRIPT/Index.js
+++ JAVASCRIPT/Index.js
@@ -1,12 +1,16 @@
-loginpage.addEventListener("submit", (event) => {
+const form = document.getElementById("loginpage");
+
+form.addEventListener('submit',(event) => {
   event.preventDefault();
+  window.location.replace("http://127.0.0.1:5500/HTML/fetching.html");
 
-  const Username = document.getElementById("Username").value.trim();
-  const PhoneNumber = document.getElementById("PhoneNubmer").value.trim();
-  const Email = document.getElementById("email").value.trim();
-  const Password = document.getElementById("password").value.trim();
+const Username = document.getElementById("Username").value.trim();
+const PhoneNumber = document.getElementById("PhoneNubmer").value.trim();
+const Email = document.getElementById("email").value.trim();
+const Password = document.getElementById("password").value.trim();
 
-  const data = {Username, PhoneNumber, Email, Password};
+const data = { Username, PhoneNumber, Email, Password };
+
   localStorage.setItem("userData", JSON.stringify(data));
 
 });
